131995f84dbd53d72b064b4357d3ca7b0a2abebd
Andrey <00andrey123@gmail.com>
2020-05-07 20:20:51 +0300

    1

---
+++ Pizzeria 1.1/Pizzeria 1.1/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Management.Instrumentation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Pizzeria2
+{
+	public abstract class Dish
+	{
+		public float mass, price;
+		protected float coefficient = (float)2.3;
+		public abstract float Getcost(int max);
+	}
+
+
+	public class Pizza : Dish
+	{
+
+		public Ingredients[] ingredients = new Ingredients[10];
+		override public float Getcost(int max)
+		{
+			float getcost = 0;
+
+
+			for (int i = 0; i < max; i++)
+			{
+				getcost += ingredients[i].Getcost();
+			}
+			return getcost * coefficient;
+
+
+
+
+		}
+
+
+
+	}
+	public class Ingredients
+	{
+		public float mass, price;
+		public string name;
+		public virtual float Getcost()
+		{
+			return mass * price;
+		}
+	}
+
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+			try
+			{
+
+
+				Pizza pizza1 = new Pizza();
+
+
+				Console.WriteLine("Введите количество ингридиентов (до 10 наименований)");
+				int max = Convert.ToInt32(Console.ReadLine());
+
+
+
+				for (int i = 0; i < max; i++)
+				{
+					pizza1.ingredients[i] = new Ingredients();
+				}
+
+
+
+
+
+
+				for (int i = 0; i < max; i++)
+				{
+					Console.WriteLine("Введите название " + (i + 1) + " -го ингридиента");
+					pizza1.ingredients[i].name = Console.ReadLine();
+					Console.WriteLine("Введите желаемую массу " + (i + 1) + " -го ингридиента");
+					pizza1.ingredients[i].mass = Convert.ToInt32(Console.ReadLine());
+					Console.WriteLine("Введите цену " + (i + 1) + " - го ингридиента");
+					pizza1.ingredients[i].price = Convert.ToInt32(Console.ReadLine());
+				}
+
+
+
+
+				Console.WriteLine("Стоимость пиццы = " + pizza1.Getcost(max) + " грн");
+				float j = 0;
+				for (int i = 0; i < max; i++)
+				{
+					j += pizza1.ingredients[i].mass;
+				}
+				Console.WriteLine("Общая масса пиццы составляет " + j + " кг");
+				Console.ReadLine();
+
+
+			}
+			catch (IndexOutOfRangeException)
+			{
+				Console.WriteLine("Я не могу обработать блюдо с больее чем 10 ингридиентов, прости((");
+				Console.ReadLine();
+			}
+
+
+
+
+		}
+	}
+}
+
---
+++ Pizzeria 1.1/Pizzeria 1.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Pizzeria 1.1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pizzeria 1.1")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("92f1ce8d-1beb-4c4b-ab62-99b4218f2b99")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
