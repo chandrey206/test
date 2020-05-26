235fb4837d7ada99b836a1a8b672681d481929c8
Andrey <00andrey123@gmail.com>
2020-05-26 19:34:02 +0300

    1

---
+++ Multiplication table tutorial/Multiplication table tutorial/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication_table_tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Random random = new Random();
+            Random random1 = new Random();
+            Console.WriteLine("Забыли таблицу умнoжения? Потренируйтесь здесь!\n");
+            int correctly = 0;
+            int wrong = 0;
+            for (; ; )
+            {
+                Console.Write("Сколько будет ");
+                int value = random.Next(1, 10);
+                int value1 = 0;
+                for (int i = 0; i < 5; i++)
+                {
+                    value1 = random1.Next(0, 10);
+                }
+                Console.WriteLine(value + " * " + value1 + "?");
+
+                if (value * value1 == Convert.ToInt32(Console.ReadLine()))
+                {
+                    Console.WriteLine("Верно! \n");
+                    correctly += 1;
+                }
+                else
+                {
+                    Console.WriteLine("Не верно!\n");
+                    Console.WriteLine("Подумайте лучше, сколько будет: " + value + " * " + value1 + "?");
+                    if (value * value1 == Convert.ToInt32(Console.ReadLine()))
+                    {
+                        Console.WriteLine("Верно! \n");
+                        correctly += 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не верно, " + value + " * " + value1 + " = " + value * value1 + "\n");
+                        wrong += 1;
+                    }
+                    wrong += 1;
+                }
+                try
+                {
+                    if (correctly + wrong == 2)
+                    {
+                        Console.WriteLine("Результат:\n правильно - " + correctly + "не правильно - " + wrong);
+                        Console.WriteLine("Нажмите 'enter' для продолжения, и дважды 'Выйти' для выхода");
+                        
+                        if (Console.ReadLine() == "Выйти") 
+                        {
+                            return;
+                        }
+                    }
+                }
+                catch (FormatException)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ Multiplication table tutorial/Multiplication table tutorial/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Multiplication table tutorial")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Multiplication table tutorial")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1839c88a-01cb-4a3a-89b8-3632a0f1f221")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
